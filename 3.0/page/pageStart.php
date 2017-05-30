8250058e0dc910e53230567f713a58a94d7c2cd3
Fabrizio <fabrizio.silvestri@yahoo.it>
2017-05-30 11:50:34 +0200

    add geocoder script

--- 3.0/page/pageStart.php
+++ 3.0/page/pageStart.php
@@ -143,6 +143,19 @@
 
         function checkSearch() {
             alert("check search");
+            var geocoder = new google.maps.Geocoder();
+            geocoder.geocode( { 'address': address}, function(results, status) {
+                if (status == 'OK') {
+                    map.setCenter(results[0].geometry.location);
+                    var marker = new google.maps.Marker({
+                        map: map,
+                    position: results[0].geometry.location
+                });
+                } else {
+                    alert('Geocode was not successful for the following reason: ' + status);
+                }
+            });
+
             getLocation();
             return true;
         }
