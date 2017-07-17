df13b229efbeaad0e01539984ec8923800a3c3b5
Fabrizio <fabrizio.silvestri@yahoo.it>
2017-07-17 15:53:24 +0200

    letti eventi homepage refactoring

--- 3.0/js/eventsListFunctions.js
+++ 3.0/js/eventsListFunctions.js
@@ -4,33 +4,37 @@ function drawEventsList() {
     text = '<div class="container-fluid bg-3 text-center">';
     text += '<div class="row">';
     max = events.length;
+    console.info(max);
     for (i = 0; i < 4; i++) {
         if(i < max) {
             text += '<div class="col-sm-3 marginBottom" id="'+events[i].id+'">';
             text += '<div class="liteBackground radiusDiv">';
             text += '<p ><h2>'+events[i].name+'</h2>';
             text += '<h4>'+events[i].day+'</h4></p>';
-            text += '<img src = "'+events[i].image+'" class="img-responsive" style = "width:100%" alt = "Image" >';
+            text += '<img src = "'+events[i].image+'" class="img-responsive" style="width:100%" alt ="Image">';
             text += '<p>'+events[i].description+'</p>';
             text += '</div></div>';
         }
     }
     text += '</div></div>';
 
-    text = '<div class="container-fluid bg-3 text-center">';
-    text += '<div class="row">';
-    for (i = 4; i < 8; i++) {
-        if(i < max) {
-            text += '<div class="col-sm-3 marginBottom" id="'+events[i].id+'">';
-            text += '<div class="liteBackground radiusDiv">';
-            text += '<p ><h2>'+events[i].name+'</h2>';
-            text += '<h4>'+events[i].day+'</h4></p>';
-            text += '<img src = "'+events[i].image+'" class="img-responsive" style = "width:100%" alt = "Image" >';
-            text += '<p>'+events[i].description+'</p>';
-            text += '</div></div>';
+    if(max > 4) {
+        text += '<div class="container-fluid bg-3 text-center">';
+        text += '<div class="row">';
+        for (i = 4; i < 8; i++) {
+            if (i < max) {
+                text += '<div class="col-sm-3 marginBottom" id="' + events[i].id + '">';
+                text += '<div class="liteBackground radiusDiv">';
+                text += '<p ><h2>' + events[i].name + '</h2>';
+                text += '<h4>' + events[i].day + '</h4></p>';
+                text += '<img src = "' + events[i].image + '" class="img-responsive" style = "width:100%" alt = "Image" >';
+                text += '<p>' + events[i].description + '</p>';
+                text += '</div></div>';
+            }
         }
+        text += '</div></div>';
     }
-    text += '</div></div>';
     console.info(text);
-    document.getElementById('eventslist').innerHTML = text;
+    if(text !== null)
+        document.getElementById('eventsList').innerHTML = text;
 }
--- 3.0/page/homepage.php
+++ 3.0/page/homepage.php
@@ -27,10 +27,13 @@
     </div>
 </div>
 <br>
-<?php include("mapDrawer.php"); ?>
-<br>
-    <div id="eventsList" onload="drawEventsList()">
+    <div class="container-fluid bg-3 text-center">
+        <div id="googleMap" class="container map"></div>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAQO1FBU7ngY0Wv20d3-gPI1sj5_ApCZ3M&callback=myMap"></script>
     </div>
+<br>
+    <div id="eventsList"></div>
+    <script>drawEventsList();</script>
 <br>
 
 <?php require("footer.php"); ?>
