d88df0e764db9012dc516cf451477e2890e245f4
wieramoroz <wieramoroz@gmail.com>
2017-07-22 19:17:16 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	3.0/admin/header.php
    #	3.0/admin/index.php
    #	3.0/admin/script/dbAccess.php

--- 3.0/admin/header.php
+++ 3.0/admin/header.php
@@ -6,9 +6,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <script src="../js/jquery.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
+    <link href="../css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="../css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+    <link href="../vendors/easypiechart/jquery.easy-pie-chart.css" rel="stylesheet" media="screen">
+    <link href="../css/styles.css" rel="stylesheet" media="screen">
+    <!--[if lte IE 8]>
+    <script language="javascript" type="text/javascript" src="../vendors/flot/excanvas.min.js"></script><![endif]-->
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="../http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <script src="../vendors/modernizr-2.6.2-respond-1.1.0.min.js"></script>
     <style>
           .navbar {
               margin-bottom: 0;
@@ -18,7 +28,7 @@
                 height: 450px
           }
           .sidenav {
-                padding-top: 20px;
+                padding-top: 0px;
                 background-color: #f1f1f1;
                 height: 100%;
           }
@@ -33,18 +43,17 @@
                 .row.content {
                     height: auto;
                 }
-
     </style>
 </head>
-<body>
 
+<body>
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <div class="navbar-header">
             <a class="navbar-brand" href="pageDashboard.php">Dashboard</a>
         </div>
         <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
+            <li class="active"><a href="index.php">Home</a></li>
             <li class="dropdown">
                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">Utenti
                     <span class="caret"></span></a>
--- 3.0/admin/pageDashboard.php
+++ 3.0/admin/pageDashboard.php
@@ -15,33 +15,32 @@
                         <li><a href="pageDeleteUser.php">DeleteUser</a></li>
                         <li><a href="pageBanUser.php">BanUser</a></li>
                         <li><a href="pageAddAdmin.php">AddAdmin</a></li>
-                   </ul>
-               </li>
-               <li>Eventi
-                   <ul>
-                       <li><a href="pageEvents.php"> Eventi</a></li>
-                       <li><a href="pageDeleteEvent.php"> DeleteEvento</a></li>
-                   </ul>
-               </li>
-           </ul>
+                    </ul>
+                </li>
+                <li>Eventi
+                    <ul>
+                        <li><a href="pageEvents.php"> Eventi</a></li>
+                        <li><a href="pageDeleteEvent.php"> DeleteEvento</a></li>
+                    </ul>
+                </li>
+            </ul>
 
-       </div>
-       <div class="col-sm-8 text-left">
-           <h1>Area Amministrativa</h1>
-           <p> TESTO DA INSERIRE</p> <!--TODO inserire descrizione delle funzionalità dell'area amministrativa-->
-           <hr>
-           <p>INSERIRE STATISTICHE</p><!--TODO inserire dei dati statistici (grafici?) del sistema-->
-       </div>
-       <div class="col-sm-2 sidenav">
-           <div class="well">
-               <p>ADS</p><!--TODO utente più attivo-->
-           </div>
-           <div class="well">
-               <p>ADS</p><!--TODO evento più seguito-->
-           </div>
-           <!--TODO se vogliamo altro-->
-       </div>
-   </div>
+        </div>
+        <div class="col-sm-8 text-left">
+            <h1>Area Amministrativa</h1>
+            <p> TESTO DA INSERIRE</p> <!--TODO inserire descrizione delle funzionalità dell'area amministrativa--><hr>
+            <p>Lorem ipsum...</p><!--TODO inserire dei dati statistici (grafici?) del sistema-->
+        </div>
+        <div class="col-sm-2 sidenav">
+            <div class="well">
+                <p>ADS</p><!--TODO se vogliamo altro-->
+            </div>
+            <div class="well">
+                <p><a href="calendar.php"> Calendar</a><!--TODO evento più seguito  Vera: Ho messo calendar--></p>
+            </div>
+        </div>
+    </div>
+</div>
 <?php
     require("shared/footer.php");
 ?>
