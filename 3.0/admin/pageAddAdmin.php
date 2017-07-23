b49884029911d9fbe466ac7ad83ced52bb32a8ab
Fabrizio <fabrizio.silvestri@yahoo.it>
2017-07-23 09:11:25 +0200

    corretto titoli area amministrativa

--- 3.0/admin/pageAddAdmin.php
+++ 3.0/admin/pageAddAdmin.php
@@ -18,13 +18,13 @@
         </div>
         <div class="col-sm-8 text-left">
             <form method="post" action="script/adminAdder.php">
-                <h1>Utenti</h1>
+                <h1>Inserisci nuovo Admin</h1>
                 <p>Inserisci le credenziali:</p>
                 <p>
-                    <input type="name" name="username" id="username" placeholder="Enter username" class="radiusDiv padding5" required>
-                    <input type="email" name="email" id="email" placeholder="Enter email" class="radiusDiv padding5" required>
-                    <input type="password" name="password" id="password" placeholder="Enter Password" class="radiusDiv padding5" required>
-                    <button type="submit" class="radiusDiv">Add</button>
+                    <input type="text" name="username" id="username" placeholder="Username" class="radiusDiv padding5" required>
+                    <input type="email" name="email" id="email" placeholder="Email" class="radiusDiv padding5" required>
+                    <input type="password" name="password" id="password" placeholder="Password" class="radiusDiv padding5" required>
+                    <input type="submit" class="radiusDiv" value="Add">
                 </p>
             </form>
         </div>
--- 3.0/admin/pageUpdateUser.php
+++ 3.0/admin/pageUpdateUser.php
@@ -17,7 +17,7 @@
                 ?>
             </div>
             <div class="col-sm-8 text-left">
-                <h1>Utenti</h1>
+                <h1>Modifica degli Utenti</h1>
                 <form method="post" action="pageUpdateUserForm.php">
                     <p>Inserisci il nome utente da modificare:</p>
                     <input type="text" name="username" placeholder="Username" class="radiusDiv padding5" required>
