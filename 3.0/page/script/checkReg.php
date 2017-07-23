10cdbf929363ea63e4758b6ed46a44df40a9b853
Loki6 <steli2001@hotmail.com>
2017-07-23 12:20:08 +0200

    checkReg, pageHomepage definitivi. (server side)

--- 3.0/page/script/checkReg.php
+++ 3.0/page/script/checkReg.php
@@ -1,40 +1,37 @@
 <?php
 
     require("dbAccess.php");
-    $error = "";
-    $username = "";
-    $password = "";
 
     try {
+        $conn = new PDO("mysql:host=$server;dbname=$dbName", $dbUser, $dbPass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $conn->prepare("INSERT INTO Users (username, password) VALUES (:username, :password);");
+
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':password', $password);
+
         $username = trim($_POST["username"]);
         $pass_pre_hash = trim($_POST["password1"]);
 
-        if ((empty($username)) || (!preg_match("/^[ -~]*$/",$username))) {
+        if ((empty($username)) || (!preg_match("/^(?=.{3,})[ -~]*$/",$username))){
             throw new Exception();
         }
 
         if(empty($pass_pre_hash)){
             throw new Exception();
         }
-        //TODO controllare espressione regolare
-        /*if (!preg_match("/^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[\W]).*$/", $pass_pre_hash)) {
-            $error = "Password non valida";
+
+        if (!preg_match("/^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[\W]).*$/", $pass_pre_hash)){
             throw new Exception();
-        }*/
-
+        }
         $password = password_hash($pass_pre_hash, PASSWORD_BCRYPT);
-
-    } catch(Exception $e) {
-        $error = "Username or password error.";
-        header("Location: ../pageRegistration.php?registerError=".$error);
-    }
-    try{
-        $conn = new PDO("mysql:host=$server;dbname=$dbName", $dbUser, $dbPass);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $stmt = $conn->prepare("INSERT INTO Users (username, password) VALUES (:username, :password);");
-        $stmt->bindParam(':username', $username);
-        $stmt->bindParam(':password', $password);
+        //libsodium extension from paragon.
+        /*$password = \Sodium\crypto_pwhash_str(
+                                                $pass_pre_hash,
+                                                \Sodium\CRYPTO_PWHASH_OPSLIMIT_INTERACTIVE,
+                                                \Sodium\CRYPTO_PWHASH_MEMLIMIT_INTERACTIVE
+                                                );*/
 
         $stmt->execute();
 
@@ -55,7 +52,14 @@
 
     }
     catch(PDOException $e){
-        $error=  "Error: " . $e->getMessage(); //for debug only ****TO BE REMOVED****
+        $conn = null;
+        //$error =  "Error: " . $e->getMessage(); //for debug only ****TO BE REMOVED****
+        $error = "error";
+        header("Location: ../pageRegistration.php?registerError=".$error);
+    }
+    catch(Exception $e) {
+        $conn = null;
+        $error = "error";
         header("Location: ../pageRegistration.php?registerError=".$error);
     }
 ?>
