3136dc881a000a97ab04b1c43d929606b8f93e49
wieramoroz <wieramoroz@gmail.com>
2017-07-23 09:17:05 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	3.0/admin/header.php
    #	3.0/admin/index.php
    #	3.0/admin/script/dbAccess.php

--- 3.0/admin/shared/navbar.php
+++ 3.0/admin/shared/navbar.php
@@ -1,21 +1,30 @@
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="pageDashboard.php">Dashboard</a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav">
-                <li class="active"><a>Utenti</a></li>
-                <li class="active"><a>Eventi</a></li>
+        <ul class="nav navbar-nav">
+            <li class="active"><a class="navbar-brand" href="pageDashboard.php">Dashboard</a></li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown">Utenti
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="pageUsers.php">Elenco utenti</a></li>
+                    <li><a href="pageUpdateUser.php">ModUser</a></li>
+                    <li><a href="pageDeleteUser.php">DeleteUser</a></li>
+                    <li><a href="pageBanUser.php">BanUser</a></li>
+                    <li><a href="pageAddAdmin.php">AddAdmin</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown">Eventi
+                <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+                <li><a href="pageEvents.php">Elenco eventi</a></li>
+                <li><a href="pageDeleteEvent.php"> DeleteEvento</a></li>
             </ul>
+            </li>
+        </ul>
+
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="script/logout.php"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
+                <li><a href="../script/logout.php"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
             </ul>
-        </div>
     </div>
 </nav>
