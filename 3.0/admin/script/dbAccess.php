1044d03ee32e9e2c980c89830b8ef0d055c09815
wieramoroz <wieramoroz@gmail.com>
2017-07-24 14:08:30 +0200

    Merge remote-tracking branch 'origin/master'
                                     # Conflicts:
                                     #	3.0/admin/header.php
                                     #	3.0/admin/index.php
                                     #	3.0/admin/script/dbAccess.php

--- 3.0/admin/script/dbAccess.php
+++ 3.0/admin/script/dbAccess.php
@@ -1,11 +1,11 @@
 <?php
-/*    $dbUser = "S4116422";
+  $dbUser = "S4116422";
   $server = "localhost";
   $dbPass = "Minsk";
-  $dbName = "S4116422";*/
+  $dbName = "S4116422";
 
-  $sename = "localhost";
+/*     $sename = "localhost";
   $dbUser = "root";
   $dbPass = "root";
-  $dbName = "sawdb";
+  $dbName = "sawdb";*/
 ?>
