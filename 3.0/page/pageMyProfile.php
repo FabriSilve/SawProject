c1135eb8d0fa51e8dcb8293c47779400f9fe0ef3
Fabrizio <fabrizio.silvestri@yahoo.it>
2017-07-26 16:35:17 +0200

    completato inserimento dati profilo e visualizzazione di questi

--- 3.0/page/pageMyProfile.php
+++ 3.0/page/pageMyProfile.php
@@ -7,11 +7,11 @@ if(!isset($logged)||!$logged)
     <script> userData = <?php require("script/userData.php"); ?>; </script>
     <div class="container text-center liteOrange radiusDiv">
         <h1>Profile</h1>
-        <h3><?php echo $_SESSION["EAusername"]; ?></h3>
+        <!--<h3><?php echo $_SESSION["EAusername"]; ?></h3>-->
     </div>
     <br>
     <div id="userData" class="container radiusDiv liteBackground">
-        <script>drawUserData(userData);</script>
     </div>
+    <script>drawUserData(userData);</script>
     <br>
 <?php require("shared/footer.php"); ?>
