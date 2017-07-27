219b6b61b2a991977a25bf230f501b34f10e87d3
Fabrizio <fabrizio.silvestri@yahoo.it>
2017-07-27 15:12:24 +0200

    modificato layout profile

--- 3.0/js/drawUserData.js
+++ 3.0/js/drawUserData.js
@@ -12,42 +12,22 @@ function drawUserData() {
     if(userData.description === null) {
         userData.description = " ";
     }
-    text = "";
 
-    text += '<div class="row">';
-    text += '   <div class="col-sm-12 text-right" id="modifyUser">';
-    text += '       <img src="../media/modify.png" id="modify" onclick="userModify();">';
-    text += '   </div>';
-    text += '</div>';
-    text += '<div class="row">';
-    text += '   <div class="row padding20">';
-    text += '       <div class="col-sm-3 text-center" id="userData1">';
-    text += '           <table class="table-responsive text-left">';
-    text += '               <tr><td>username:</td><td class="padding5">'+userData.username+'</td></tr>';
-    text += '               <tr><td>nome:</td><td class="padding5">'+userData.name+'</td></tr>';
-    text += '               <tr><td>cognome:</td><td class="padding5">'+userData.surname+'</td></tr>';
-    text += '               <tr><td>residenza:</td><td class="padding5">'+userData.residence+'</td></tr>';
-    text += '           </table>';
-    text += '       </div>';
-    text += '       <div class="col-sm-6 text-center" id="userData2">';
-    text += '           <h5>'+userData.description+'</h5>';
+    document.getElementById("username").innerHTML = userData.username;
+    document.getElementById("name").innerHTML = "name <h3>"+userData.name+"</h3>";
+    document.getElementById("surname").innerHTML = "surname <h3>"+userData.surname+"</h3>";
+    document.getElementById("residence").innerHTML = "residence <h3>"+userData.residence+"</h3>";
     if(userData.link !== "") {
-        text += '       <p class="text-center"><a href="' + userData.link + '" target="blank">link</a></p>';
+        document.getElementById("link").innerHTML = '<a href="'+userData.link+'" target="blank"><img src="../media/social.png"></a>';
+    } else {
+        document.getElementById("link").innerHTML = '';
     }
-    text += '       </div>';
-    text += '       <div class="col-sm-3 text-center">';
-    text += '           <table class="table-responsive text-center">';
-    text += '               <tr><td>'+userData.events+'</td><td class="padding5"> :eventi inseriti</td></tr>';  //TODO implement
-    text += '               <tr><td>'+userData.fans+'</td><td class="padding5"> :fans</td></tr>';  //TODO implement
-    text += '               <tr><td>'+userData.followed+'</td><td class="padding5"> :segue</td></tr>';  //TODO implement
-    text += '               <tr><td>'+userData.signaled+'</td><td class="padding5"> :segnalati</td></tr>';  //TODO implement
-    text += '           </table>';
-    text += '       </div>';
-    text += '   </div>';
-    text += '</div>';
+    document.getElementById("description").innerHTML = "Description: <h5>"+userData.description+"</h5>";
 
-
-    document.getElementById('userData').innerHTML = text;
+    document.getElementById("events").innerHTML = "insert events: "+userData.events;
+    document.getElementById("fans").innerHTML = "fans: "+userData.fans;
+    document.getElementById("followed").innerHTML = "followed: "+userData.followed;
+    document.getElementById("signaled").innerHTML = "signaled: "+userData.signaled;
 }
 
 function userModify() {
@@ -58,18 +38,18 @@ function userModify() {
     btn = '<img src="../media/save.png" onclick="confirmUserModify();">';
     document.getElementById("modifyUser").innerHTML = btn;
 
-    userData1  = '<table class="table-responsive text-left">';
-    userData1 += '    <tr><td>username:</td><td class="padding5">'+userData.username+'</td></tr>';
-    userData1 += '    <tr><td>nome:</td><td class="padding5"><input name="name" id="name" placeholder="Name" class="radiusDiv" type="text" value="'+userData.name+'"></td></tr>';
-    userData1 += '    <tr><td>cognome:</td><td class="padding5"><input name="surname" id="surname" placeholder="Surname" class="radiusDiv" type="text" value="'+userData.surname+'"></td></tr>';
-    userData1 += '    <tr><td>residenza:</td><td class="padding5"><input name="residence" id="residence" placeholder="Residence" class="radiusDiv" type="text" value="'+userData.residence+'"></td></tr>';
-    userData1 += '</table>';
-    document.getElementById("userData1").innerHTML = userData1;
+    text  = '<div class="col-sm-6 text-center">';
+    text += '<div class="row"><input name="name" id="name" placeholder="Name" class="radiusDiv padding5" type="text" value="'+userData.name+'"></div>';
+    text += '<div class="row"><input name="surname" id="surname" placeholder="Surname" class="radiusDiv padding5" type="text" value="'+userData.surname+'"></div>';
+    text += '<div class="row"><input name="residence" id="residence" placeholder="Residence" class="radiusDiv padding5" type="text" value="'+userData.residence+'"></div>';
+    text += '<div class="row"><input type="url" name="link" id="link" placeholder="Link Social" class="radiusDiv padding5" value="'+userData.link+'"></div>';
+    text += '</div>';
+    text += '<div class="col-sm-6 text-center">';
+    text += '<textarea cols="50" rows="6" name="description" id="description" placeholder="Description" class="radiusDiv padding5">'+userData.description+'</textarea>';
+    text += '</div>';
 
-    userData2  = '<textarea cols="50" rows="6" name="description" id="description" placeholder="Description" class="radiusDiv">'+userData.description+'</textarea><br>';
-    userData2 += '<input type="url" name="link" id="link" placeholder="Link Social" class="radiusDiv padding5" value="'+userData.link+'">';
-
-    document.getElementById("userData2").innerHTML = userData2;
+    console.info(text);
+    document.getElementById("userData").innerHTML = text;
 
 
 }
