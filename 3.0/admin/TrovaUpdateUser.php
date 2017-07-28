09d83c7426bfa79fba39312b5afb33110cabf68d
Loki6 <steli2001@hotmail.com>
2017-07-28 16:14:16 +0200

    parte admin edit users completata

--- 3.0/admin/TrovaUpdateUser.php
+++ 3.0/admin/TrovaUpdateUser.php
@@ -75,10 +75,8 @@ require("shared/navbar.php");
                            class="radiusDiv padding5" value="<?php echo $email; ?>">
                     <input type="text" name="newUsername" id="newUsername" placeholder="New Username"
                            class="radiusDiv padding5" required>
-                    <input type="email" name="newEmail" id="newEmail" placeholder="New Email" class="radiusDiv padding5"
-                           required>
-                    <input type="password" name="newPassword" id="newPassword" placeholder="New Password" class="radiusDiv padding5"
-                           required>
+                    <input type="email" name="newEmail" id="newEmail" placeholder="New Email" class="radiusDiv padding5">
+                    <input type="password" name="newPassword" id="newPassword" placeholder="New Password" class="radiusDiv padding5">
                     <p><br><input type="submit" value="Confirmation"></p>
                 </form>
             </div>
--- 3.0/admin/script/userMod.php
+++ 3.0/admin/script/userMod.php
@@ -4,6 +4,10 @@ require("dbAccess.php");
 
 $message = "";
 try {
+    if (!isset($_POST["newEmail"]) || !isset($_POST["newUsername"]) || !isset($_POST["newPassword"])){
+        $message = "No fields were changed.";
+        throw new Exception();
+    }
     $username = trim($_POST['username']);
     $email = trim($_POST['email']);
     $newUsername = trim($_POST['newUsername']);
@@ -22,61 +26,67 @@ try {
         throw new Exception();
     }*/
 
-    if(!empty($newUsername) || preg_match("/^[A-Za-z][A-Za-z0-9]{3,}$/", $newUsername)){
-        $stmt = $conn->prepare("UPDATE Users SET  username = :newUsername WHERE username = :username");
-        $stmt->bindParam(":username", $username);
-        $stmt->bindParam(":newUsername", $newUsername);
-        $stmt->execute();
-
-        $stmt = $conn->prepare("UPDATE Profiles SET  username = :newUsername WHERE username = :username");
-        $stmt->bindParam(":username", $username);
-        $stmt->bindParam(":newUsername", $newUsername);
-        $stmt->execute();
-    }else {
-        $message = "New username is invalid";
-    }
-
-    if(!empty($newEmail) || preg_match("/[a-z0-9_]+@[a-z0-9\-]+\.[a-z0-9\-\.]+$]/", $newEmail)){
-        if(empty($newUsername)){
-            $stmt = $conn->prepare("UPDATE Profiles SET email = :newEmail WHERE username = :username");
+    if(!empty($newUsername)){
+        if (preg_match("/^[A-Za-z][A-Za-z0-9]{3,}$/", $newUsername)) {
+            $stmt = $conn->prepare("UPDATE Users SET  username = :newUsername WHERE username = :username");
             $stmt->bindParam(":username", $username);
-            $stmt->bindParam(":newEmail", $newEmail);
-            $stmt->execute();
-        }else {
-            $stmt = $conn->prepare("UPDATE Profiles SET email = :newEmail WHERE username = :newUsername");
             $stmt->bindParam(":newUsername", $newUsername);
-            $stmt->bindParam(":newEmail", $newEmail);
-            $stmt->execute();
-        }
-    }else{
-        $message = "New email is invalid";
-    }
-
-    if(!empty($newPassword) || preg_match("/^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[\W]).*$/", $newPassword)){
-        if(empty($newUsername)) {
-            $stmt = $conn->prepare("DELETE FROM Users WHERE username = :username");
-            $stmt->bindParam(":username", $username);
             $stmt->execute();
 
-            $stmt = $conn->prepare("INSERT INTO Users (username, password) VALUES (:username, :newPassword);");
+            $stmt = $conn->prepare("UPDATE Profiles SET  username = :newUsername WHERE username = :username");
             $stmt->bindParam(":username", $username);
-            $stmt->bindParam(":newPassword", $newPassword);
+            $stmt->bindParam(":newUsername", $newUsername);
+            $stmt->execute();
         }
         else{
-            $stmt = $conn->prepare("DELETE FROM Users WHERE username = newUsername");
-            $stmt->bindParam(":newUsername", $newUsername);
-            $stmt->execute();
-
-            $stmt = $conn->prepare("INSERT INTO Users (username, password) VALUES (:newUsername, :newPassword);");
-            $stmt->bindParam(":newUsername", $newUsername);
-            $stmt->bindParam(":newPassword", $newPassword);
+            $message = "New username is invalid";
+            throw new Exception();
         }
-    }else{
-        $message = "New password is invalid";
     }
 
-    if(!empty($message))
-        throw new Exception();
+    if(!empty($newEmail)) {
+        if (preg_match("/[a-z0-9_]+@[a-z0-9\-]+\.[a-z0-9\-\.]+$]/", $newEmail)) {
+            if (empty($newUsername)) {//TODO controllare presenza mail da sistema
+                $stmt = $conn->prepare("UPDATE Profiles SET email = :newEmail WHERE username = :username");
+                $stmt->bindParam(":username", $username);
+                $stmt->bindParam(":newEmail", $newEmail);
+                $stmt->execute();
+            } else {
+                $stmt = $conn->prepare("UPDATE Profiles SET email = :newEmail WHERE username = :newUsername");
+                $stmt->bindParam(":newUsername", $newUsername);
+                $stmt->bindParam(":newEmail", $newEmail);
+                $stmt->execute();
+            }
+        } else {
+            $message = "New email is invalid";
+            throw new Exception();
+        }
+    }
+
+    if(!empty($newPassword)){
+        if (preg_match("/^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[\W]).*$/", $newPassword)) {
+            if (empty($newUsername)) {
+                $stmt = $conn->prepare("DELETE FROM Users WHERE username = :username");
+                $stmt->bindParam(":username", $username);
+                $stmt->execute();
+
+                $stmt = $conn->prepare("INSERT INTO Users (username, password) VALUES (:username, :newPassword);");
+                $stmt->bindParam(":username", $username);
+                $stmt->bindParam(":newPassword", $newPassword);
+            } else {
+                $stmt = $conn->prepare("DELETE FROM Users WHERE username = newUsername");
+                $stmt->bindParam(":newUsername", $newUsername);
+                $stmt->execute();
+
+                $stmt = $conn->prepare("INSERT INTO Users (username, password) VALUES (:newUsername, :newPassword);");
+                $stmt->bindParam(":newUsername", $newUsername);
+                $stmt->bindParam(":newPassword", $newPassword);
+            }
+        }else{
+            $message = "New password is invalid";
+            throw new Exception();
+        }
+    }
 
     $conn->commit();
     $message = "User was successfully modified";
